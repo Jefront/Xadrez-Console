b3ddd8596c980549ada4b0b8b75dfe243dcfa277
Jefferson <Jeffersoncostadacruz8@gmail.com>
2024-01-30 19:49:58 -0300

    Começando a adicionar peças no tabuleiro

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
 
 namespace Xadrez
 {
@@ -7,10 +8,17 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
-            Posicao P;
-            P = new Posicao(3, 4);
+            Tabuleiro tab = new Tabuleiro(8,8);
 
-            Console.WriteLine("Posição: "+P);
+            tab.colocarPeca(new Torre(tab,Cor.Preta), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab,Cor.Preta), new Posicao(1, 3));
+            tab.colocarPeca(new Rei(tab,Cor.Preta), new Posicao(2, 4));
+
+
+
+
+            Tela.ImprimirTabuleiro(tab);
+            Console.ReadLine();
         }
     }
 }
