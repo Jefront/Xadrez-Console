bf5b0611f0cf09be287ee654aff6b139531d6e4c
Jefferson <Jeffersoncostadacruz8@gmail.com>
2024-01-31 18:42:24 -0300

    Adicionando a classe posição xadrez

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -8,25 +8,10 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
 
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
-
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
-
-
-
-
-                Tela.ImprimirTabuleiro(tab);
-                Console.ReadLine();
-            }
-            catch (TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.toPosicao());
         }
     }
 }
--- Xadrez/obj/Debug/net8.0/Xadrez.AssemblyInfo.cs
+++ Xadrez/obj/Debug/net8.0/Xadrez.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Xadrez")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3ddd8596c980549ada4b0b8b75dfe243dcfa277")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+253d05c16f6880d802e55b59b2df15f52aa255b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Xadrez")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Xadrez")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
