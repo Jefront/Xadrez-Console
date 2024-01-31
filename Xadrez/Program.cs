253d05c16f6880d802e55b59b2df15f52aa255b0
Jefferson <Jeffersoncostadacruz8@gmail.com>
2024-01-31 18:13:34 -0300

    Tratando Exceções

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -8,17 +8,25 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8,8);
 
-            tab.colocarPeca(new Torre(tab,Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab,Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab,Cor.Preta), new Posicao(2, 4));
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
+
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
 
 
 
-            Tela.ImprimirTabuleiro(tab);
-            Console.ReadLine();
+                Tela.ImprimirTabuleiro(tab);
+                Console.ReadLine();
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
--- Xadrez/obj/Debug/net8.0/Xadrez.AssemblyInfo.cs
+++ Xadrez/obj/Debug/net8.0/Xadrez.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Xadrez")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b554d9344d6702d1686251cf867b2658f860d0ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3ddd8596c980549ada4b0b8b75dfe243dcfa277")]
 [assembly: System.Reflection.AssemblyProductAttribute("Xadrez")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Xadrez")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
